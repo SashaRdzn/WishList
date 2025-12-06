accf010cf2bee23bb6969cdc5036bf83169e3f13
Sasha_rdzn <rudzinskijsasa@gmail.com>
2025-12-06 20:50:36 +0300

    dep

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,7 +37,13 @@ app.use(cors({
   maxAge: 86400
 }));
 
-app.options('*', cors());
+app.options('/*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
